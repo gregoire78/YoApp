16a191fe2f4dd345ba2b64851bb216ba5fc0542f
Grégoire <greg.joncour@gmail.com>
2017-02-20 16:29:51 +0100

    game

--- app/src/main/java/xyz/gregoirejoncour/merdouille2/Principale.java
+++ app/src/main/java/xyz/gregoirejoncour/merdouille2/Principale.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -36,8 +35,10 @@ public class Principale extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                Intent intent = new Intent(Principale.this, Game.class);
+                startActivity(intent);
             }
         });
     }
@@ -75,4 +76,9 @@ public class Principale extends AppCompatActivity {
         intent.putExtra(MESSAGE, message);
         startActivity(intent);
     }
+
+    /*public void jouer(View view) {
+        Intent intent = new Intent(this, Game.class);
+        startActivity(intent);
+    }*/
 }
