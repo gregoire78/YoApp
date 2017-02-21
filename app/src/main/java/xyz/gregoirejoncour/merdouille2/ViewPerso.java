8e2f44d8fe40fbf704cafab81af7cd7abbf5ac87
Grégoire <greg.joncour@gmail.com>
2017-02-21 10:28:06 +0100

    move!

--- app/src/main/java/xyz/gregoirejoncour/merdouille2/ViewPerso.java
+++ app/src/main/java/xyz/gregoirejoncour/merdouille2/ViewPerso.java
@@ -1,10 +1,13 @@
 package xyz.gregoirejoncour.merdouille2;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -15,9 +18,20 @@ public class ViewPerso extends View {
 
     //pinceau
     private Paint p = new Paint();
+    private Bitmap logo = null;
+    private float xOri = 0, yOri = 0;
+    int largImage, hautImage;
+
+    private float deltaX = 0, deltaY = 0;
+    boolean move = false;
 
     public ViewPerso(Context context, AttributeSet attributeSet) {
         super(context, attributeSet);
+
+        BitmapDrawable d = (BitmapDrawable) getResources().getDrawable(R.drawable.sncf);
+        logo = d.getBitmap();
+        largImage = logo.getWidth();
+        hautImage = logo.getHeight();
     }
 
     public void onDraw(Canvas canvas) {
@@ -26,7 +40,9 @@ public class ViewPerso extends View {
         p.setColor(Color.BLACK);
         p.setStyle(Paint.Style.FILL);
 
-        canvas.drawRect(0,0, getWidth(), getHeight(), p);
+        canvas.drawRect(0, 0, getWidth(), getHeight(), p);
+
+        canvas.drawBitmap(logo, xOri, yOri, p);
 
         // texte blanc
         p.setColor(Color.WHITE);
@@ -35,6 +51,32 @@ public class ViewPerso extends View {
         p.setTextAlign(Paint.Align.CENTER);
 
         String texte = getResources().getString(R.string.app_name);
-        canvas.drawText(texte, getWidth()/2, getHeight()/2, p);
+        canvas.drawText(texte, getWidth() / 2, getHeight() / 2, p);
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        int action = event.getAction();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                deltaX = event.getX() - xOri;
+                deltaY = event.getY() - yOri;
+
+                if (deltaX >= 0 && deltaX <= largImage
+                        && deltaY >= 0 && deltaY <= hautImage) {
+                    move = true;
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (move) {
+                    xOri = event.getX() - deltaX;
+                    yOri = event.getY() - deltaY;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                move = false;
+                break;
+        }
+        invalidate();
+        return true;
     }
 }
